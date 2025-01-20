758fc53fe4be6f9f16c3551f453cd1a25d0be4d6
jiruneko <oohikaru310@gmail.com>
2025-01-21 08:52:56 +0900

    third-commit

---
+++ basic1/base10.py
@@ -0,0 +1,16 @@
+car = {"brand": "Toyota", "model": "Prius", "year": 2015, 1: 100}
+
+print(car["brand"])
+print(car.get("brand"))
+
+print(car.get(1))
+
+print(car.keys())  # キー一覧
+print(car.values())  # 値一覧
+print(car.items())  # キー ＋ 値
+
+for k, v in car.items():
+    print("key = {}, value = {}".format(k, v))
+
+if "brand" in car:
+    print("carのブランドは{}".format(car["brand"]))
